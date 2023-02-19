ca9ccd95b4ffb94cba33a40fa4e9358d126629c9
Philip Henderson <philip.henderson@hotmail.com>
2023-02-19 03:28:05 -0500

    Inital Commit

---
+++ Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+    public Vector3 eulerRotation;
+    public float damper;
+
+    void Start()
+    {
+        transform.eulerAngles = eulerRotation;        
+    }
+    
+    void Update()
+    {
+        if (target == null)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, damper + Time.deltaTime);
+    }
+}
